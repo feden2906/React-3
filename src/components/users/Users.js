53331f9eb10ccee8dbf469669e5b627be3210bed
LiraSenega <kurukuzik@gmail.com>
2021-08-12 14:42:40 +0300

    users

---
+++ src/components/users/Users.js
@@ -0,0 +1,11 @@
+import {usersList} from "../data-file";
+
+export default function UsersList() {
+return (
+        <div>
+            {
+                usersList.map()
+            }
+        </div>
+    );
+}
