1c4e72b25540a06d9b31708be13c84c73b036270
LiraSenega <kurukuzik@gmail.com>
2021-08-31 19:50:41 +0300

    trial form

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ControlledForm from "./components/controlledFrom/ControlledForm";
+
+
+export default function App() {
+    return (
+        <div>
+            <ControlledForm/>
+        </div>
+    );
+}
